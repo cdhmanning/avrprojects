c6372aa8a045034c194709cf5190d16ceaf484cc
Charles Manning <cdhmanning@gmail.com>
2009-05-10 15:03:28 +1200

    Update colour sensor and README.

--- colour-sensor/main.c
+++ colour-sensor/main.c
@@ -9,41 +9,95 @@
 #include "myusart.h"
 
 #define SENSOR_SLAVE_ADDRESS 0x02
+#define NCHANNELS	4
+#define NCOLOURS	4 /* ambient, red, blue, green */
+
+/* 
+ * LED control on PORTB
+ */
+ 
+#define RED_PIN		(1<<5)
+#define GREEN_PIN	(1<<4)
+#define BLUE_PIN	(1<<3)
+
+#define CHANNEL0_PIN	(1<<0)
+#define CHANNEL1_PIN	(1<<1)
+#define CHANNEL2_PIN	(1<<2)
+#define CHANNEL3_PIN	(1<<6)
+
+
 const char SensorVersion[8] = "1.1";
 const char SensorName[8] = "MBOYS";
 const char SensorType[8] = "COLOUR4";
 
 
-struct {
-	uint16_t rawVal;
-	uint16_t offset;
-	uint16_t scale;
-} samples[NCHANNELS][3];
+uint8_t rawValue[NCHANNELS][NCOLOURS];
 
+static uint8_t state;
+static uint8_t channel;
+static uint8_t rgb;
 
 
 
 void SampleProcess(void)
 {
-	static uint16_t a,b,c;
-	static uint8_t state;
-	static uint8_t channel;
-	static uint8_t rgb;
+	static uint8_t a,b,c;
+	
+	uint16_t sample_value;
+	uint8_t start_sample = 0;
 	
 	if(ADCSRA & 0x40)
 		return;
+
 	if(channel >= NCHANNELS)
 		state = 0;
-	if(rgb >= 3)
+	if(rgb >= NCOLOURS)
 		state = 0;
 	
-	start_sample = 0;
-	
-	sample_value = (ADCH << 8) | (ADCL);
+	sample_value = ADCH;
 	
 	switch(state){
+		case 0:
+			DDRB = 0;
+			break;
 		case 1: /* Enable LED and do a wasted sample */
-			// TODO enable LED
+		
+			
+			switch(rgb){
+				case 0: DDRB = 0;
+					PORTB = 0;
+					break;
+				case 1:
+					DDRB = RED_PIN;
+					PORTB = RED_PIN;
+					break;
+				case 2:
+					DDRB = GREEN_PIN;
+					PORTB = GREEN_PIN;
+					break;
+				case 3: 
+					DDRB = BLUE_PIN;
+					PORTB = BLUE_PIN;
+					break;
+				default:
+					DDRB = 0;
+			}
+			
+			switch(channel){
+				case 0:
+					DDRB |= CHANNEL0_PIN;
+					break;
+				case 1:
+					DDRB |= CHANNEL1_PIN;
+					break;
+				case 2:
+					DDRB |= CHANNEL2_PIN;
+					break;
+				case 3:
+					DDRB |= CHANNEL3_PIN;
+					break;
+			}
+						
 			start_sample = 1;
 			state++;
 			break;
@@ -67,52 +121,56 @@ void SampleProcess(void)
 			state++;
 			break;
 		case 5: /* Save third value and advance to next sample */
-			if(sample > b)
+			if(sample_value > b)
 				c = b;
-			else if(sample < a)
+			else if(sample_value < a)
 				c =a;
 			else
-				c = sample;
-			samples[channel][rgb].rawVal = c;
+				c = sample_value;
+
+			rawValue[channel][rgb] = c;
 			
 			start_sample = 1;
 			rgb++;
-			if(rgb >= 3){
+			if(rgb > NCOLOURS){
 				rgb = 0;
 				channel++;
 			}
 			state = 1;
 			break;
 	}
-
-}
-
-void
-ADCProcess (void)
-{
-	if (!(ADCSRA & 0x40)) {
-
-		// Conversion not in progress
-		if (curChan < N_ADC)
-			adcVal[curChan] = ADCH;
-		curChan++;
-		if (curChan >= N_ADC)
-			curChan = 0;
-
-		// Start next conversion
-		ADMUX = 0x20 | curChan;	// External AVcc Ref, left justify, set mux
-		ADCSRA = 0x87;	// Enable, set slowest clock
-		ADCSRA = 0xC7;	// Enable, start conversion set slowest clock
+	
+	if(start_sample){
+		ADMUX = 0x20 | channel;	// External AVcc Ref, left justify, set mux
+		ADCSRA = 0x85;	// Enable, set clock
+		ADCSRA = 0xC5;	// Enable, start conversion set clock
 	}
+
 }
 
 void
 Spin (void)
 {
-	ADCProcess ();
+	static uint16_t xx;
+	xx++;
+	
+	SampleProcess ();
+
+	if(xx == 0 && state == 0){
+		
+		usart_TxQueuePutDec(rawValue[0][0]);
+		usart_TxQueuePutDec(rawValue[0][1]);
+		usart_TxQueuePutDec(rawValue[0][2]);
+		usart_TxQueuePutDec(rawValue[0][3]);
+		usart_TxQueuePutStr("\n\r");
+		
+		rgb = 0;
+		channel = 0;
+		state = 1;
+	}
 } 
 
-static unsigned char messageBuf[6];
+static unsigned char messageBuf[16];
 
 void
 ProcessMessage (void)
@@ -120,16 +178,14 @@ ProcessMessage (void)
 
 	/* Have received a message */
 	
-	intuart_TxQueuePutStr("\n\rMsg:");
+	usart_TxQueuePutStr("\n\rMsg:");
 
-	intuart_TxQueuePutHex(messageBuf[0]);
-	intuart_TxQueuePutHex(messageBuf[1]);
+	usart_TxQueuePutHex(messageBuf[0]);
+	usart_TxQueuePutHex(messageBuf[1]);
 	
 	/* Command received */
-	if (messageBuf[0] == 0x40)
-		DDRB = messageBuf[1];
-	if (messageBuf[0] == 0x41)
-		PORTB = messageBuf[1];
+	if (messageBuf[0] == 0x40){
+	}
 
 	/*
 	 * Info request received
@@ -140,16 +196,11 @@ ProcessMessage (void)
 		twi_sl_update(SensorName, sizeof (SensorName));
 	} else if (messageBuf[0] == 0x10) {
 		twi_sl_update(SensorType, sizeof (SensorType));
-	} else if (messageBuf[0] == 0x18) {
-		messageBuf[0] = adcVal[0];
-		messageBuf[1] = adcVal[1];
-		messageBuf[2] = adcVal[2];
-		messageBuf[3] = adcVal[3];
-		messageBuf[4] = PINB;
-		twi_sl_update(messageBuf, 5);
+	} else if (messageBuf[0] == 0x20) {
+		twi_sl_update(rawValue, sizeof(rawValue));
 	}
 
-	intuart_TxQueuePutStr("\n\r");
+	usart_TxQueuePutStr("\n\r");
 }
 
 static unsigned char slaveAddress;
@@ -166,8 +217,8 @@ main (void)
 	// Init timer
 	Timer0Initialise ();
 	
-	intuart_Init(12);
-	intuart_TxQueuePutStr("\n\r"
+	usart_Init(12);
+	usart_TxQueuePutStr("\n\r"
 			      "MBOYS COLOUR sensor\n\r"
 			      "-------------------\n\r");
 	
@@ -179,14 +230,13 @@ main (void)
 	DDRD = 0;
 	PINB = 0;
 	PINC = 0;
-	PIND = 0xFF;
+	PIND = 0;
 
 	// Own TWI slave address
 	slaveAddress = SENSOR_SLAVE_ADDRESS;
 
 	// Initialise TWI module for slave operation. Include address and/or enable General Call.
 	twi_init(slaveAddress<<1);
-
 	interrupt_Enable(1);
 
 	// Start the TWI transceiver to enable reseption of the first command from the TWI Master.
@@ -203,7 +253,7 @@ main (void)
 		counter++;
 		if(counter> 50000){
 			counter = 0;
-			intuart_TxQueuePut('.');
+//			usart_TxQueuePut('.');
 		}
 	}
 }
